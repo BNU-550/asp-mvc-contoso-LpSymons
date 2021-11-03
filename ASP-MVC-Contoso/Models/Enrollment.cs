3172328e59921ea41979cdc656751d3857c5ca8e
LpSymons <71932780+LpSymons@users.noreply.github.com>
2021-11-03 07:56:17 +0000

    Add project files.

---
+++ ASP-MVC-Contoso/Models/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿using ASP_MVC_Contoso.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_Contoso.Models
+{
+
+    public enum Grades
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No Grade")]
+        public Grades? Grade { get; set; }
+
+        // Navigation proerties
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
+
